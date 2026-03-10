fa70a9decc34ba1864f59aa0a26043d89e4ad638
NewcomerATL <andersontirza@gmail.com>
2026-03-10 18:46:41 +0700

    [GREEN] Implement Payment model to pass tests

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
@@ -9,21 +9,25 @@ public class Payment {
     private Map<String, String> paymentData;
 
     public Payment(String id, String method, String status, Map<String, String> paymentData) {
+        this.id = id;
+        this.method = method;
+        this.status = status;
+        this.paymentData = paymentData;
     }
 
     public String getId() {
-        return null;
+        return id;
     }
 
     public String getMethod() {
-        return null;
+        return method;
     }
 
     public String getStatus() {
-        return null;
+        return status;
     }
 
     public Map<String, String> getPaymentData() {
-        return null;
+        return paymentData;
     }
 }
