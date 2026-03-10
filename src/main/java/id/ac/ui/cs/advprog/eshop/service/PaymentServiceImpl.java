91a1f8a04ceb4c70916796a75aead5787f78ebc4
NewcomerATL <andersontirza@gmail.com>
2026-03-10 19:34:41 +0700

    [GREEN] Implement OrderService integration inside PaymentService

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/PaymentServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/PaymentServiceImpl.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 
 @Service
 public class PaymentServiceImpl implements PaymentService {
@@ -15,23 +16,61 @@ public class PaymentServiceImpl implements PaymentService {
     @Autowired
     private PaymentRepository paymentRepository;
 
+    @Autowired
+    private OrderService orderService;
+
     @Override
     public Payment addPayment(Order order, String method, Map<String, String> paymentData) {
-        return null;
+        String paymentId = UUID.randomUUID().toString();
+
+        paymentData.put("orderId", order.getId());
+
+        Payment payment = new Payment(paymentId, method, paymentData);
+        paymentRepository.save(payment);
+
+        if ("SUCCESS".equals(payment.getStatus())) {
+            orderService.updateStatus(order.getId(), "SUCCESS");
+        } else if ("REJECTED".equals(payment.getStatus())) {
+            orderService.updateStatus(order.getId(), "FAILED");
+        }
+
+        return payment;
     }
 
     @Override
     public Payment setStatus(Payment payment, String status) {
+        Payment savedPayment = paymentRepository.findById(payment.getId());
+
+        if (savedPayment != null) {
+            if (!status.equals("SUCCESS") && !status.equals("REJECTED")) {
+                throw new IllegalArgumentException("Status must be SUCCESS or REJECTED");
+            }
+
+            Payment updatedPayment = new Payment(savedPayment.getId(), savedPayment.getMethod(), status, savedPayment.getPaymentData());
+            paymentRepository.save(updatedPayment);
+
+            String orderId = savedPayment.getPaymentData().get("orderId");
+
+            if (orderId != null) {
+                if (status.equals("SUCCESS")) {
+                    orderService.updateStatus(orderId, "SUCCESS"); // Cocok dengan OrderStatus.SUCCESS
+                } else if (status.equals("REJECTED")) {
+                    orderService.updateStatus(orderId, "FAILED");  // Cocok dengan OrderStatus.FAILED
+                }
+            }
+
+            return updatedPayment;
+        }
         return null;
     }
 
     @Override
     public Payment getPayment(String paymentId) {
-        return null;
+        return paymentRepository.findById(paymentId);
     }
 
     @Override
     public List<Payment> getAllPayments() {
-        return null;
+        return paymentRepository.findAll();
     }
 }
